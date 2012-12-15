3049bd4462c8cf047f76877bbcd1fe5250109267
Arnout Engelen <arnouten@bzzt.net>
2012-12-15 12:35:36 +0100

    Apply peterz-raw_pagefault_disable.patch from patches-3.6.10-rt22

--- mm/memory.c
+++ mm/memory.c
@@ -3497,6 +3497,7 @@ unlock:
 	return 0;
 }
 
+#ifdef CONFIG_PREEMPT_RT_FULL
 void pagefault_disable(void)
 {
 	inc_preempt_count();
@@ -3525,6 +3526,7 @@ void pagefault_enable(void)
 	preempt_check_resched();
 }
 EXPORT_SYMBOL_GPL(pagefault_enable);
+#endif
 
 /*
  * By the time we get here, we already hold the mm semaphore
